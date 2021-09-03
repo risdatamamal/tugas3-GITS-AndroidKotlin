c364e6470c21dbde18d2393c72a2d160fa4fcc9f
Risda Tamam <risda123tamam@gmail.com>
2021-09-03 18:53:51 +0700

    Update Main.kt

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,4 +1,15 @@
-fun main(args: Array<String>) {
-    println("Hello World!")
-    
+fun main() {
+
+    for (number in 1..20)
+    {
+        if (number == 5)
+        {
+            continue
+        }
+        else if (number % 11 == 0)
+        {
+            break
+        }
+        println("Number is $number")
+    }
 }
